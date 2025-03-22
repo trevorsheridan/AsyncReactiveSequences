0adcc764ae05df6be0053ea774830dbc89de91a2
Trevor Sheridan <trevorsheridan@users.noreply.github.com>
2025-03-22 13:30:31 -0600

    Testing making AnyAsyncSequence its own iterator that has the proper next(isolation:) function

--- Sources/Sequences/ErasedTypes/AnyAsyncIterator.swift
+++ Sources/Sequences/ErasedTypes/AnyAsyncIterator.swift
@@ -5,17 +5,31 @@
 //  Created by Trevor Sheridan on 8/28/24.
 //
 
-public struct AnyAsyncIterator<Element>: AsyncIteratorProtocol {
-    private var closure: () async throws -> Element?
-    
-    public init<Iterator: AsyncIteratorProtocol>(_ iterator: Iterator) where Iterator.Element == Element {
-        closure = {
-            var iterator = iterator
-            return try await iterator.next()
-        }
-    }
-    
-    public func next() async throws -> Element? {
-        try await closure()
-    }
-}
+import Synchronization
+
+//public final class AnyAsyncIterator<Element: Sendable>: AsyncIteratorProtocol, Sendable {
+//    typealias Closure = (_ actor: (any Actor)?) async throws -> Element?
+//    
+//    private let iterator: Mutex<any AsyncIteratorProtocol>
+//    
+//    public init<Iterator: AsyncIteratorProtocol>(_ iterator: Iterator) where Iterator.Element == Element {
+//        self.iterator = .init(iterator)
+////        self.iterator = .init({ actor in
+////            var iterator = iterator
+////            
+////            if let actor {
+////                return try await iterator.next(isolation: actor)
+////            } else {
+////                return try await iterator.next()
+////            }
+////        })
+//    }
+//    
+//    public func next() async throws -> Element? {
+////        try await closure(nil)
+//    }
+//    
+//    public func next(isolation actor: isolated (any Actor)?) async throws(any Error) -> Element? {
+////        try await closure(actor)
+//    }
+//}
--- Sources/Sequences/ErasedTypes/AnyAsyncSequence.swift
+++ Sources/Sequences/ErasedTypes/AnyAsyncSequence.swift
@@ -5,17 +5,28 @@
 //  Created by Trevor Sheridan on 8/28/24.
 //
 
-public struct AnyAsyncSequence<Element>: AsyncSequence, Sendable {
-    private var closure: @Sendable () -> AnyAsyncIterator<Element>
+import Synchronization
+
+public final class AnyAsyncSequence<Element: Sendable>: AsyncSequence, AsyncIteratorProtocol, Sendable {
+    private let iterator: @Sendable (_ isolation: (any Actor)?) async throws -> Element?
     
     public init<Sequence: AsyncSequence>(_ base: Sequence) where Sequence.Element == Element, Sequence: Sendable {
-        closure = {
-            AnyAsyncIterator<Element>(base.makeAsyncIterator())
+        iterator = { @Sendable actor in
+            var iterator = base.makeAsyncIterator()
+            return try await iterator.next(isolation: actor)
         }
     }
     
-    public func makeAsyncIterator() -> AnyAsyncIterator<Element> {
-        closure()
+    public func next() async throws -> Element? {
+        try await iterator(#isolation)
+    }
+    
+    public func next(isolation actor: isolated (any Actor)?) async throws(any Error) -> Element? {
+        try await iterator(actor)
+    }
+    
+    public nonisolated func makeAsyncIterator() -> AnyAsyncSequence<Element> {
+        self
     }
 }
 
