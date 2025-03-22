cb6c8834d74b93927c6775088e7a5937dc5095e9
Trevor Sheridan <trevorsheridan@users.noreply.github.com>
2025-03-22 16:12:26 -0600

    Reimplement AnyAsyncSequence with next(isolation:) (#4)

--- Sources/Sequences/ErasedTypes/AnyAsyncSequence.swift
+++ Sources/Sequences/ErasedTypes/AnyAsyncSequence.swift
@@ -5,17 +5,31 @@
 //  Created by Trevor Sheridan on 8/28/24.
 //
 
-public struct AnyAsyncSequence<Element>: AsyncSequence, Sendable {
-    private var closure: @Sendable () -> AnyAsyncIterator<Element>
+public final class AnyAsyncSequence<Element>: AsyncSequence, Sendable {
+    private let sequence: any AsyncSequence & Sendable
     
     public init<Sequence: AsyncSequence>(_ base: Sequence) where Sequence.Element == Element, Sequence: Sendable {
-        closure = {
-            AnyAsyncIterator<Element>(base.makeAsyncIterator())
+        sequence = base
+    }
+    
+    public struct Iterator: AsyncIteratorProtocol {
+        var iterator: any AsyncIteratorProtocol
+        
+        init(sequence: AnyAsyncSequence) {
+            iterator = sequence.sequence.makeAsyncIterator()
+        }
+        
+        public mutating func next() async throws -> Element? {
+            try await iterator.next() as! Element?
+        }
+        
+        public mutating func next(isolation actor: isolated (any Actor)? = #isolation) async throws -> Element? {
+            try await iterator.next(isolation: actor) as! Element?
         }
     }
     
-    public func makeAsyncIterator() -> AnyAsyncIterator<Element> {
-        closure()
+    public func makeAsyncIterator() -> Iterator {
+        Iterator(sequence: self)
     }
 }
 
