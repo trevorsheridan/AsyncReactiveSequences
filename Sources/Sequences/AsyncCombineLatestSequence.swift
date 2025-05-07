65388286ff57f47cb592fab817fb89bc32684f41
Trevor Sheridan <trevorsheridan@users.noreply.github.com>
2025-05-07 06:39:08 -0600

    Test not using .sink

--- Sources/Sequences/AsyncCombineLatestSequence.swift
+++ Sources/Sequences/AsyncCombineLatestSequence.swift
@@ -20,7 +20,7 @@ where repeat each Upstream: AsyncSequence & Sendable, repeat (each Upstream).Ele
         observe(upstream: repeat each upstream)
     }
     
-    private func observe(upstream: repeat each Upstream) {
+    private func observe(isolation actor: isolated (any Actor)? = #isolation, upstream: repeat each Upstream) {
         var index = 0
         for sequence in repeat each upstream {
             obvserve(sequence: sequence, at: index).store(in: &cancellables)
@@ -28,21 +28,32 @@ where repeat each Upstream: AsyncSequence & Sendable, repeat (each Upstream).Ele
         }
     }
     
-    private func obvserve<Sequence>(sequence: Sequence, at index: Int) -> TaskCancellable where Sequence: AsyncSequence & Sendable, Sequence.Element: Sendable {
-        sequence.sink { [weak self] value in
-            self?.perform(with: value, index: index)
-        }
+    private func obvserve<Sequence>(isolation actor: isolated (any Actor)? = #isolation, sequence: Sequence, at index: Int) -> TaskCancellable where Sequence: AsyncSequence & Sendable, Sequence.Element: Sendable {
+        TaskCancellable(task: .init { [weak self] in
+            guard let self else { return }
+            for try await value in sequence {
+                perform(with: value, index: index)
+            }
+        }, mode: .automatic)
+        
+//        sequence.sink { [weak self] value in
+//            self?.perform(with: value, index: index)
+//        }
     }
     
     private func perform<V>(isolation actor: isolated (any Actor)? = #isolation, with value: V, index: Int) {
         let value = (repeat (each container).replace(index: index, value: value))
         
+        print("Performing")
+        
         for value in repeat each value {
             if let value = value as? OptionalValue, value.isNil {
+                print("Bailing", value.isNil)
                 return
             }
         }
         
+        print("Sending", value)
         downstream.send(value as? Element)
     }
     
